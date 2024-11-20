e0d5dacdac5537d887498d767de0ebc2f4725dd2
amothm80 <amo.1980@gmail.com>
2024-11-20 07:25:57 +0300

    trying async

--- src/events.js
+++ src/events.js
@@ -1,8 +1,15 @@
 import { checkReponse, buildLoaders, showError } from './index.js';
+
+// async function getWeatherDataAsync(url){
+//   const resp = fetch(url, { method: 'GET', headers:{}}).then((response)=>{return response.json()}).then((json)=>{return json})
+//   resp;
+// }
+
+
  function getWeatherData() {
   const location = document.querySelector('#location-input');
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.value}?unitGroup=metric&key=ULFYVQLBQ6GTY39PCZVMKXJBC&contentType=json`;
-
+  // getWeatherDataAsync(url)
   setTimeout(() => {
      fetch(url, {
       method: 'GET',
